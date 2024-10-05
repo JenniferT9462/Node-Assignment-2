8a494ca1bc1562f00c41ac87b485ff069d883a3c
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-04 22:29:23 -0500

    fixed typos and format

--- survey.js
+++ survey.js
@@ -1,10 +1,12 @@
 import { input, select} from '@inquirer/prompts';
 import { say } from 'cowsay';
+// import * as emoji from 'node-emoji'
 
 import { favoriteCostume } from './questions/favorite-costume.js';
 import { favoriteCandy } from './questions/favorite-candy.js';
 import { favoriteHorrorMovie } from './questions/favorite-horror-movie.js';
 
+console.log(`🎃 Welcome to the Spooktacular Halloween Survey! 🎃`);
 const name = await input({message: 'What is your name?'});
 console.log(say({text: `Hello ${name}`, e: "o-"}));
 
@@ -12,8 +14,10 @@ const costume = await select(favoriteCostume);
 const candy = await select(favoriteCandy);
 const movie = await select(favoriteHorrorMovie);
 
-console.log("🎃 Welcome to the Spooktacular Halloween Survey! 🎃");
-console.log(`/nThanks ${name}, for sharing your spooky favorites! Here's you Halloween profile:`);
+
+
+
+console.log(`\nThanks ${name}, for sharing your spooky favorites! Here's your Halloween profile:`);
 console.log(`- Favorite Costume: ${costume}`);
 console.log(`- Favorite Candy: ${candy}`);
 console.log(`- Favorite Horror Movie: ${movie}`);
