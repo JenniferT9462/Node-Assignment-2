b59cbdf5c49a778fafd3f45860e058883b4f4378
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-10-05 22:33:45 -0500

    add boxen and chalk to profile message

--- survey.js
+++ survey.js
@@ -1,24 +1,36 @@
+//Module imports
 import { input, select} from '@inquirer/prompts';
 import { say } from 'cowsay';
+import chalk  from 'chalk';
 // import * as emoji from 'node-emoji'
+import boxen from 'boxen';
 
+//Survey questions from questions folder(directory)
 import { favoriteCostume } from './questions/favorite-costume.js';
 import { favoriteCandy } from './questions/favorite-candy.js';
 import { favoriteHorrorMovie } from './questions/favorite-horror-movie.js';
 
-console.log(`🎃 Welcome to the Spooktacular Halloween Survey! 🎃`);
-const name = await input({message: 'What is your name?'});
-console.log(say({text: `Hello ${name}`, e: "o-"}));
+//Setting up chalk colors for usage
+const halloween = chalk.hex('#FFA500'); //Orange color
+const profileMessage = chalk.bold.greenBright;
+// const favorites = chalk.bold.red;
 
+
+
+//Greeting and name display with cowsay
+console.log(halloween(`🎃 Welcome to the Spooktacular Halloween Survey! 🎃`));
+const name = await input({message: 'What is your name?'});
+console.log(say({text: `${name}, I'v vant to suck vour blood!!`, e: ". .", T: "vv"}));
+
+//Get the user selections
 const costume = await select(favoriteCostume);
 const candy = await select(favoriteCandy);
 const movie = await select(favoriteHorrorMovie);
 
+//After Survey is complete display Halloween profile
+console.log(profileMessage(`\n${name}, Thank You for sharing your spooky Halloween favorites! Here's your profile:`));
+console.log(boxen(`${costume}`, {title: 'Favorite Costume', titleAlignment: 'center'}));
+console.log(boxen(`${candy}`, {title: 'Favorite Candy', titleAlignment: 'center'}));
+console.log(boxen(`${movie}`, {title: 'Favorite Horror Movie', titleAlignment: 'center'}));
+console.log(halloween(`Remember, the best way to celebrate Halloween is with candy, costumes, and a good scare! 👻`));
 
-
-
-console.log(`\nThanks ${name}, for sharing your spooky favorites! Here's your Halloween profile:`);
-console.log(`- Favorite Costume: ${costume}`);
-console.log(`- Favorite Candy: ${candy}`);
-console.log(`- Favorite Horror Movie: ${movie}`);
-console.log(`Remember, the best way to celebrate Halloween is with candy, costumes, and a good scare! 👻`);
